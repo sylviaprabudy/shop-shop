142ab2a09aab9b5d5d51ece0319b3b2830fd777f
Sylvia <sylviaprabudy@gmail.com>
2020-10-14 22:02:08 -0500

    add cart function

--- client/src/pages/Detail.js
+++ client/src/pages/Detail.js
@@ -5,66 +5,100 @@ import { useQuery } from '@apollo/react-hooks';
 import { QUERY_PRODUCTS } from "../utils/queries";
 import spinner from '../assets/spinner.gif'
 import { useStoreContext } from "../utils/GlobalState";
-import { UPDATE_PRODUCTS } from "../utils/actions";
+import {
+    REMOVE_FROM_CART,
+    UPDATE_CART_QUANTITY,
+    ADD_TO_CART,
+    UPDATE_PRODUCTS,
+} from '../utils/actions';
+import Cart from '../components/Cart';
 
 function Detail() {
-  const [state, dispatch] = useStoreContext();
-  const { id } = useParams();
-  
-  const [currentProduct, setCurrentProduct] = useState({})
-  
-  const { loading, data } = useQuery(QUERY_PRODUCTS);
-  
-  const { products } = state;
-  
-  useEffect(() => {
-    if (products.length) {
-      setCurrentProduct(products.find(product => product._id === id));
-    } else if (data) {
-      dispatch({
-        type: UPDATE_PRODUCTS,
-        products: data.products
-      });
-    }
-  }, [products, data, dispatch, id]);
+    const [state, dispatch] = useStoreContext();
+    const { id } = useParams();
 
-  return (
-    <>
-      {currentProduct ? (
-        <div className="container my-1">
-          <Link to="/">
-            ← Back to Products
-          </Link>
+    const [currentProduct, setCurrentProduct] = useState({})
 
-          <h2>{currentProduct.name}</h2>
+    const { loading, data } = useQuery(QUERY_PRODUCTS);
 
-          <p>
-            {currentProduct.description}
-          </p>
+    const { products, cart } = state;
 
-          <p>
-            <strong>Price:</strong>
-            ${currentProduct.price}
-            {" "}
-            <button>
-              Add to Cart
-            </button>
-            <button>
-              Remove from Cart
-            </button>
-          </p>
+    const addToCart = () => {
+        const itemInCart = cart.find((cartItem) => cartItem._id === id);
 
-          <img
-            src={`/images/${currentProduct.image}`}
-            alt={currentProduct.name}
-          />
-        </div>
-      ) : null}
-      {
-        loading ? <img src={spinner} alt="loading" /> : null
-      }
-    </>
-  );
+        if (itemInCart) {
+            dispatch({
+                type: UPDATE_CART_QUANTITY,
+                _id: id,
+                purchaseQuantity: parseInt(itemInCart.purchaseQuantity) + 1
+            });
+        } else {
+            dispatch({
+                type: ADD_TO_CART,
+                product: { ...currentProduct, purchaseQuantity: 1 }
+            });
+        }
+    };
+
+    const removeFromCart = () => {
+        dispatch({
+          type: REMOVE_FROM_CART,
+          _id: currentProduct._id
+        });
+      };
+
+    useEffect(() => {
+        if (products.length) {
+            setCurrentProduct(products.find(product => product._id === id));
+        } else if (data) {
+            dispatch({
+                type: UPDATE_PRODUCTS,
+                products: data.products
+            });
+        }
+    }, [products, data, dispatch, id]);
+
+    return (
+        <>
+            {currentProduct ? (
+                <div className="container my-1">
+                    <Link to="/">
+                        ← Back to Products
+                    </Link>
+
+                    <h2>{currentProduct.name}</h2>
+
+                    <p>
+                        {currentProduct.description}
+                    </p>
+
+                    <p>
+                        <strong>Price:</strong>
+                        ${currentProduct.price}
+                        {" "}
+                        <button>
+                            Add to Cart
+                        </button>
+                        <button 
+                        disabled={!cart.find(p => p._id === currentProduct._id)} 
+                        onClick={removeFromCart}
+                        >
+                        Remove from Cart
+                        </button>
+                    </p>
+
+                    <img
+                        src={`/images/${currentProduct.image}`}
+                        alt={currentProduct.name}
+                    />
+                </div>
+            ) : null}
+            {
+                loading ? <img src={spinner} alt="loading" /> : null
+            }
+            <Cart />
+        </>
+    );
 };
 
 export default Detail;

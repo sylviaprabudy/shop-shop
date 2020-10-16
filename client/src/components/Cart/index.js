bb7a6954d9eb6c142fbf93e1adbe2082ee9cbb85
Sylvia <sylviaprabudy@gmail.com>
2020-10-16 15:15:40 -0500

    finish adding stripe

--- client/src/components/Cart/index.js
+++ client/src/components/Cart/index.js
@@ -1,25 +1,40 @@
 import React, { useEffect } from 'react';
 import CartItem from '../CartItem';
 import Auth from '../../utils/auth';
-import './style.css';
+
 import { useStoreContext } from '../../utils/GlobalState';
 import { TOGGLE_CART, ADD_MULTIPLE_TO_CART } from "../../utils/actions";
 import { idbPromise } from "../../utils/helpers";
+import { QUERY_CHECKOUT } from '../../utils/queries';
+import { loadStripe } from '@stripe/stripe-js';
+import { useLazyQuery } from '@apollo/react-hooks';
+import './style.css';
+
+const stripePromise = loadStripe('pk_test_TYooMQauvdEDq54NiTphI7jx');
 
 const Cart = () => {
     const [state, dispatch] = useStoreContext();
+    const [getCheckout, { data }] = useLazyQuery(QUERY_CHECKOUT);
+    
+    useEffect(() => {
+        if (data) {
+          stripePromise.then((res) => {
+            res.redirectToCheckout({ sessionId: data.checkout.session });
+          });
+        }
+      }, [data]);
 
     useEffect(() => {
         async function getCart() {
-          const cart = await idbPromise('cart', 'get');
-          dispatch({ type: ADD_MULTIPLE_TO_CART, products: [...cart] });
+            const cart = await idbPromise('cart', 'get');
+            dispatch({ type: ADD_MULTIPLE_TO_CART, products: [...cart] });
         };
-      
+
         if (!state.cart.length) {
-          getCart();
+            getCart();
         }
-      }, [state.cart.length, dispatch]);
-      
+    }, [state.cart.length, dispatch]);
+
     function toggleCart() {
         dispatch({ type: TOGGLE_CART });
     }
@@ -30,6 +45,18 @@ const Cart = () => {
             sum += item.price * item.purchaseQuantity;
         });
         return sum.toFixed(2);
+    }
+
+    function submitCheckout() {
+        const productIds = [];
+        getCheckout({
+            variables: { products: productIds }
+        });
+        state.cart.forEach((item) => {
+            for (let i = 0; i < item.purchaseQuantity; i++) {
+                productIds.push(item._id);
+            }
+        });
     }
 
     if (!state.cartOpen) {
@@ -54,15 +81,15 @@ const Cart = () => {
                         <strong>Total: ${calculateTotal()}</strong>
                         {
                             Auth.loggedIn() ?
-                                <button>
+                                <button onClick={submitCheckout}>
                                     Checkout
-                                </button>
+                          </button>
                                 :
                                 <span>(log in to check out)</span>
-                                }
-                            </div>
-                        </div>
-                    ) : (
+                        }
+                    </div>
+                </div>
+            ) : (
                     <h3>
                         <span role="img" aria-label="shocked">
                             😱
